138e142e821a062a6afe5e52c9109c0955612342
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2023-06-14 01:05:03 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ Authors: Addison Farley, RJ Trenchard, Mason Sain, Will Castillo, Noah Lanctot
 
 Version 1.0
 
-This application runs a script every 24 hours that will pull relevant energy data from the Energy Information Administration (EIA) API, process and clean it, then will send it to a BigQuery database for storage.
+License: MIT
+
+This application runs a script every 24 hours that will pull relevant energy data from the Energy Information
+Administration (EIA) API, process and clean it, then will send it to a BigQuery database for storage.
 """
 
 import time
 import subprocess
 
 def run_script():
-    #run energy script
-    subprocess.run(['python', 'solv4solvx.py'])
+    # run energy script
+    subprocess.run(['python', 'query_database.py'])
 
 while True:
     run_script()
-    #sleep for 24 hours (24 * 60 * 60 seconds)
+    # sleep for 24 hours (24 * 60 * 60 seconds)
     time.sleep(24 * 60 * 60)
+
